fe91b267c5849bc8f7f14ae41751dad69951310c
Shark43 <talia4343@gmail.com>
2018-12-26 17:55:31 +0100

    add compatibility for express

--- node-dispatcher-with-check-token.js
+++ node-dispatcher-with-check-token.js
@@ -557,3 +557,491 @@ MongoND.prototype.replaceOne = function(req, res, dbName, dbColletion, query, er
 };
 
 module.exports.Mongo= new MongoND();
+
+
+//EXPRESS
+
+/**
+ * @callback mongoCallback
+ * @param {object} req
+ * @param {object} res
+ * @param {object} error
+ * @param {object} data
+ * @param {object} client
+ */
+const UtilitiesExpress = function() {
+    this.prompt = 'Dispatch >> >> >> ';
+    this.list = {'GET': {}, 'POST': {}, 'DELETE': {}, 'PUT': {}, 'PATCH': {}};
+};
+
+UtilitiesExpress.prototype.errorListener = function(req, res, next) {
+    const resource = url.parse(req.url, true).pathname;
+
+    if (resource.substr(0, 4) == '/api') {
+        this.sendErrorString(req, res, next);
+    } else {
+        fs.readFile('./static/error.html', (err, data) => {
+            if (err) {
+                this.sendErrorString(req, res, next);
+            } else {
+                res.set({'Content-Type': 'text/html;charset=utf-8'});
+                res.status(200);
+                res.send(data);
+            }
+        });
+    }
+};
+/**
+ * for 404 error
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {object} headers
+ */
+UtilitiesExpress.prototype.sendErrorString = function(req, res, next, headers) {
+    headers = (headers !== null) ? headers : {'Content-Type': 'text-plain;charset=UTF-8'};
+    res.status(404);
+    res.set(headers);
+    res.send('Risorsa non trovata');
+};
+
+UtilitiesExpress.prototype.staticListener = function(req, res, next) {
+    let risorsa = url.parse(req.url, true).pathname;
+
+    if (risorsa == '/') {
+        risorsa = '/index.html';
+    }
+    const fileName = './static' + risorsa;
+
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
+            this.errorListener(req, res, next);
+        } else {
+            const header = {'Content-Type': mime.getType(fileName)+';charset=utf-8'};
+            res.status(200);
+            res.set(header);
+            res.send(data);
+        }
+    });
+};
+
+
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {Error} err
+ * @param {object} headers
+ */
+UtilitiesExpress.prototype.sendError = function(req, res, next, err, headers) {
+    const header = (headers !== null) ? headers : {'Content-Type': 'text/plain;charset=utf-8'};
+    res.status(err.code);
+    res.set(header);
+    res.send(err.message);
+};
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {Error} err
+ * @param  {object} data
+ * @param {object} headers
+ */
+UtilitiesExpress.prototype.sendJson = function(req, res, next, err, data, headers) {
+    if (err && 'error' in err && err['error']) {
+        this.sendError(req, res, next, err);
+    } else {
+        const header = (headers !== null) ? headers : {'Content-Type': 'application/json'};
+        res.status(200);
+        res.set(header);
+        res.send(data);
+    }
+};
+/**
+ * @param  {object} request - server request
+ * @return {object} cookies - key : value
+ */
+UtilitiesExpress.prototype.parseCookies = function(request) {
+    const list = {};
+    const rc = request.headers.cookie;
+
+    rc && rc.split(';').forEach(function( cookie ) {
+        const parts = cookie.split('=');
+        list[parts.shift().trim()] = decodeURI(parts.join('='));
+    });
+
+    return list;
+};
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {string} path
+ * @param  {object} headers
+ */
+UtilitiesExpress.prototype.sendPage = function(req, res, next, path, headers) {
+    fs.readFile(path, 'UTF-8', (err, page) => {
+        const header = (headers) ? headers : {'Content-Type': 'text/html;charset=utf-8'};
+        res.set(header);
+        res.status(err ? 404 : 200);
+        err ? res.send() : res.send(page);
+    });
+};
+/**
+ * @param  {object} data
+ * @param  {number} exp
+ * @param  {string} signature
+ * @return {string} jwt
+ */
+UtilitiesExpress.prototype.generateToken = function(data, exp, signature) {
+    if (!exp) {
+        exp = Math.floor(Date.now() / 1000) + 60;
+    }
+    return jwt.sign({...data, exp}, signature);
+};
+/**
+ * @param  {string} path
+ * @param  {string} encoding
+ * @return {readFileSync} objectFile
+ */
+UtilitiesExpress.prototype.readFileSync = function(path, encoding) {
+    return fs.existsSync(path) ? {'file': fs.readFileSync(path, encoding), 'error': 0}
+        : {'error': 1, 'message': `file doesn't exists at this ${path}`, 'file': ''};
+};
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {string} signature
+ * @param {string} regenerationTime
+ * @return {object} result
+ */
+/**
+ * @param  {object} req - server request
+ * @return {string} token - string that contains token
+ */
+UtilitiesExpress.prototype.getLoginToken = function(req) {
+    const cookie = this.parseCookies(req);
+    if (cookie && 'token' in cookie) {
+        return cookie['token'];
+    } else {
+        return '';
+    }
+};
+/**
+* @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {string} signature - privateKey
+ * @param  {int} regenerationTime
+ * @return {Error} error - if there insn't error object have token property
+ */
+UtilitiesExpress.prototype.checkToken = function(req, res, next, signature, regenerationTime) {
+    const token = this.getLoginToken(req);
+    if (!token && token != '') {
+        return {error: 1, message: 'missing token', code: 401};
+    } else {
+        return jwt.verify(token, signature, (error, data) => {
+            if (error) {
+                return {error: 1, message: 'fail match', code: 403};
+            } else {
+                regenerationTime = regenerationTime ? regenerationTime : Math.floor(Date.now() / 1000) + 60;
+                const token = this.generateToken(data, regenerationTime, signature);
+                return {error: 0, token, code: 200};
+            }
+        });
+    }
+};
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {string} firstString
+ * @param  {string} secondString
+ * @param  {function} callback
+ */
+UtilitiesExpress.prototype.bcryptCompare = function(req, res, next, firstString, secondString, callback) {
+    bcrypt.compare(firstString, secondString, (err, result) => {
+        if (err) {
+            this.sendError(req, res, next, {code: 500, message: 'errore: bcrypt compare', error: 1});
+        } else {
+            if (!result) {
+                this.sendError(req, res, next, {code: 401, message: 'errore: Password non valida', error: 1});
+            } else {
+                /** un ora di validita */
+                callback(req, res, next, firstString, secondString);
+            }
+        }
+    });
+};
+
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {string} privateKey - privateKey
+ * @param  {verifyTokenSendResponse} callback - (req, res, result)
+ */
+UtilitiesExpress.prototype.verifyTokenSendResponse = function(req, res, next, privateKey, callback) {
+    const result = this.checkToken(req, res, next, privateKey);
+    if (result === null) {
+        this.sendError(req, res, next, {'error': 1, 'message': 'undefined token error', 'code': 500});
+    } else {
+        if ('code' in result) {
+            switch (result['code']) {
+            case 200:
+                // res.setHeader('Set-Cookie', 'token=' + result['token'] + ';max-age=' + (60 * 60 * 24 * 3)+';Path=/');
+                res.cookie('token', result['token'], {'path': '/', 'max-age': (60 * 60 * 24 * 3)});
+                callback(req, res, result);
+                break;
+            default:
+            case 401:
+                this.sendJson(req, res, next, {code: 401, message: 'must have token', error: true});
+                break;
+            case 403:
+                this.sendJson(req, res, next, {code: 403, message: 'bad token', error: true});
+                break;
+            case 500:
+                this.sendJson(req, res, next, {code: 500, message: 'internal server error', error: true});
+                break;
+            }
+        } else {
+            this.sendJson(req, res, next, {code: 500, message: 'internal server error tkn', error: true});
+        }
+    }
+};
+
+module.exports.UtilitiesExpress = new UtilitiesExpress();
+
+
+const MongoExpress = function() {
+    this.uri = '';
+};
+/**
+ * @param  {string} uri
+ */
+MongoExpress.prototype.setUri = function setUri(uri) {
+    if (uri) {
+        this.uri = uri;
+    } else {
+        this.uri = '';
+    }
+};
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {getConnection} callback - req,res,callback
+ */
+MongoExpress.prototype.getConnection = function(req, res, next, callback) {
+    if (this.uri == '') {
+        UtilitiesExpress.prototype.sendError.call(MongoExpress, req, res, next, {code: '500', message: 'errore: manca la stringa di connesione al connesione al db', error: 1});
+    } else {
+        mongoClient.connect(this.uri, {useNewUrlParser: true}, function(err, client) {
+            if (err) {
+                UtilitiesExpress.prototype.sendError.call(MongoExpress, req, res, next, {code: '500', message: 'errore connesione al db', error: 1});
+            } else {
+                callback(req, res, next, client);
+            }
+        });
+    }
+};
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {string} dbName - database name
+ * @param  {string} dbColletion - database collection name
+ * @param  {object} query - query object
+ * @param {bool} errorHandling - bool for errorHandling
+ * @param  {mongoCallback} callback - callback (req, res, err, data, client)
+ */
+MongoExpress.prototype.getFind = function(req, res, next, dbName, dbColletion, query, errorHandling, callback) {
+    this.getConnection(req, res, next, (req, res, next, client) => {
+        const db = client.db(dbName);
+        const collection = db.collection(dbColletion);
+        const find = (query && 'find' in query) ? query['find'] : {};
+        const sort = (query && 'sort' in query) ? query['sort'] : {};
+        const limit = (query && 'limit' in query) ? query['limit'] : 0;
+        const skip = (query && 'skip' in query) ? query['skip'] : 0;
+        const project = (query && 'project' in query) ? query['project'] : {};
+
+        collection.find(find).project(project).sort(sort).skip(skip).limit(limit).toArray(function(err, data) {
+            if (errorHandling && err) {
+                UtilitiesExpress.prototype.sendError.call(MongoExpress, req, res, next, {
+                    code: '500', message: 'errore esequzione find', error: 1,
+                });
+                client.close();
+            } else {
+                callback(req, res, next, err, data, client);
+            }
+        });
+    });
+};
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {string} dbName - database name
+ * @param  {string} dbColletion - database collection name
+ * @param  {object} query - query object
+ * @param {bool} errorHandling - bool for errorHandling
+ * @param  {mongoCallback} callback - callback (req, res, err, data, client)
+ */
+MongoExpress.prototype.getFindOne = function(req, res, next, dbName, dbColletion, query, errorHandling, callback) {
+    this.getConnection(req, res, next, (req, res, next, client) => {
+        const db = client.db(dbName);
+        const collection = db.collection(dbColletion);
+
+        collection.findOne(query, function(err, result) {
+            if (errorHandling && err) {
+                UtilitiesExpress.prototype.sendError.call(MongoExpress, req, res, next, {
+                    code: '500', message: 'errore esequzione findOne', error: 1,
+                });
+                client.close();
+            } else {
+                callback(req, res, next, err, result, client);
+            }
+        });
+    });
+};
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {string} dbName - database name
+ * @param  {string} dbColletion - database collection name
+ * @param  {object} query - query object
+ * @param {bool} errorHandling - bool for errorHandling
+ * @param  {mongoCallback} callback - callback (req, res, err, data, client)
+ */
+MongoExpress.prototype.getAggregate = function(req, res, next, dbName, dbColletion, query, errorHandling, callback) {
+    this.getConnection(req, res, next, (req, res, next, client) => {
+        const db = client.db(dbName);
+        const collection = db.collection(dbColletion);
+
+        collection.aggregate(query).toArray(function(err, data) {
+            if (errorHandling && err) {
+                UtilitiesExpress.prototype.sendError.call(MongoExpress, req, res, next, {
+                    code: '500', message: 'errore esequzione aggregate', error: 1,
+                });
+                client.close();
+            } else {
+                callback(req, res, next, err, data, client);
+            }
+        });
+    });
+};
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {string} dbName - database name
+ * @param  {string} dbColletion - database collection name
+ * @param  {object} query - query object
+ * @param {bool} errorHandling - bool for errorHandling
+ * @param  {mongoCallback} callback - callback (req, res, err, data, client)
+ */
+MongoExpress.prototype.getDelete = function(req, res, next, dbName, dbColletion, query, errorHandling, callback) {
+    this.getConnection(req, res, next, (req, res, next, client) => {
+        const db = client.db(dbName);
+        const collection = db.collection(dbColletion);
+
+        collection.removeMany(query, function(err, data) {
+            if (errorHandling && err) {
+                UtilitiesExpress.prototype.sendError.call(MongoExpress, req, res, next, {
+                    code: '500', message: 'errore esequzione removeMany', error: 1,
+                });
+                client.close();
+            } else {
+                callback(req, res, next, err, data, client);
+            }
+        });
+    });
+};
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {string} dbName - database name
+ * @param  {string} dbColletion - database collection name
+ * @param  {object} query - query object
+ * @param {bool} errorHandling - bool for errorHandling
+ * @param  {mongoCallback} callback - callback (req, res, err, data, client)
+ */
+MongoExpress.prototype.insertOne = function(req, res, next, dbName, dbColletion, query, errorHandling, callback) {
+    this.getConnection(req, res, next, (req, res, next, client) => {
+        const db = client.db(dbName);
+        const collection = db.collection(dbColletion);
+
+        collection.insertOne(query, function(err, data) {
+            if (errorHandling && err) {
+                UtilitiesExpress.prototype.sendError.call(MongoExpress, req, res, next, {
+                    code: '500', message: 'errore esequzione insertOne', error: 1,
+                });
+                client.close();
+            } else {
+                callback(req, res, next, err, data, client);
+            }
+        });
+    });
+};
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {string} dbName - database name
+ * @param  {string} dbColletion - database collection name
+ * @param  {object} query - query object
+ * @param {bool} errorHandling - bool for errorHandling
+ * @param  {mongoCallback} callback - callback (req, res, err, data, client)
+ */
+MongoExpress.prototype.updateMany = function(req, res, next, dbName, dbColletion, query, errorHandling, callback) {
+    this.getConnection(req, res, next, (req, res, next, client) => {
+        const db = client.db(dbName);
+        const collection = db.collection(dbColletion);
+        const filter = (query && 'filter' in query) ? query['filter'] : {};
+        const action = (query && 'action' in query) ? query['action'] : {};
+
+        collection.updateMany(filter, action, function(err, data) {
+            if (errorHandling && err) {
+                UtilitiesExpress.prototype.sendError.call(MongoExpress, req, res, next, {
+                    code: '500', message: 'errore esequzione updateMany', error: 1,
+                });
+                client.close();
+            } else {
+                callback(req, res, next, err, data, client);
+            }
+        });
+    });
+};
+/**
+ * @param  {object} req - server request
+ * @param  {object} res - server response
+ * @param  {object} next - server next
+ * @param  {string} dbName - database name
+ * @param  {string} dbColletion - database collection name
+ * @param  {object} query - query object
+ * @param {bool} errorHandling - bool for errorHandling
+ * @param  {mongoCallback} callback - callback (req, res, err, data, client)
+ */
+MongoExpress.prototype.replaceOne = function(req, res, next, dbName, dbColletion, query, errorHandling, callback) {
+    this.getConnection(req, res, next, (req, res, next, client) => {
+        const db = client.db(dbName);
+        const collection = db.collection(dbColletion);
+        const filter = (query && 'filter' in query) ? query['filter'] : {};
+        const newDocument = (query && 'newDocument' in query) ? query['newDocument'] : {};
+        collection.replaceOne(filter, newDocument, {'upsert': true}, function(err, data) {
+            if (errorHandling && err) {
+                UtilitiesExpress.prototype.sendError.call(MongoExpress, req, res, next, {
+                    code: '500', message: 'errore esequzione replaceOne', error: 1,
+                });
+                client.close();
+            } else {
+                callback(req, res, next, err, data, client);
+            }
+        });
+    });
+};
+
+module.exports.MongoExpress= new MongoExpress();
